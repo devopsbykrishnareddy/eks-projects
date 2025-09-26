a0a65f3695358e49828bf32796543b09a1fa84f6
Krishna <devopswithkrishnareddy@gmail.com>
2025-09-26 09:09:16 +0530

    rds with eks

---
+++ RDS-EKS/output.tf
@@ -0,0 +1,15 @@
+output "cluster_id" {
+  value = aws_eks_cluster.devopsbykrishna.id
+}
+
+output "node_group_id" {
+  value = aws_eks_node_group.devopsbykrishna.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.devopsbykrishna_vpc.id
+}
+
+output "subnet_ids" {
+  value = aws_subnet.devopsbykrishna_subnet[*].id
+}
